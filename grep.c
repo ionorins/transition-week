f4f1aacab64a753f015ca477c2e04c937160dcfa
ionorins <ion.orins@modex.tech>
2020-10-05 16:16:32 +0100

    last

--- grep.c
+++ grep.c
@@ -1,39 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-
-// copied from https://stackoverflow.com/questions/11546791/what-is-the-best-hash-function-for-rabin-karp-algorithm 
-inline unsigned hash ( void *key, int len )
-{
-    unsigned long char *p = key;
-    unsigned long h = 0;
-    int i;
-
-    for ( i = 0; i < len; i++ )
-        h = 33 * h + p[i];
-
-    return h;
-}
-
-inline static bool search(char const *const s1, 
-               char const *const s2)
-{
-	size_t const n1 = strlen(s1);
-	size_t const n2 = strlen(s2);
-    unsigned long hsub = hash(s2, n2);
-    unsigned long hs   = hash(s1, n1);
-    size_t   nmax = n2 - n1;
-
-    for (size_t i = 0; i < nmax; ++i) {
-        if (hs == hsub) {
-            if (strncmp(&s1[i], s2, i + n2 - 1) == 0)
-                return true;
-        }
-        hs = hash(&s1[i + 1], i + n2);
-    }
-    return false;
-}
 
 int main(int argc, char **argv)
 {
@@ -42,7 +9,7 @@ int main(int argc, char **argv)
 	ssize_t read;
 
 	while ((read = getline(&line, &len, stdin)) != -1)
-		if (search(line, argv[1]))
+		if (strstr(line, argv[1]))
 			printf("%s", line);
 
 	free(line);
