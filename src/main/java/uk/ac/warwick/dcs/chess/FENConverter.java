d4403fecc2d2035cd031b4d3b256ea5d86b3c99c
Andrew Cox <ac@jnahcox.me.uk>
2020-10-09 16:21:32 +0100

    board to FEN util

---
+++ src/main/java/uk/ac/warwick/dcs/chess/FENConverter.java
@@ -0,0 +1,55 @@
+package uk.ac.warwick.dcs.chess;
+
+import uk.ac.warwick.dcs.chess.piece.*;
+
+public class FENConverter {
+    public FENConverter() {
+    }
+
+    public static String convert(Board board, Player player) {
+        StringBuilder FEN = new StringBuilder();
+
+//        iterate through rows
+        for (int y = 0; y < 8; y++) {
+//            keep count of blanks on row
+            int blanks = 0;
+
+            for (int x = 0; x < 8; x++) {
+                ChessPiece piece = board.pieceAtLocation(x, y);
+
+                if (piece == null) {
+                    blanks++;
+                } else {
+//                    end of blanks, append and reset counter
+                    if (blanks > 0) {
+                        FEN.append(blanks);
+                        blanks = 0;
+                    }
+
+                    if (piece instanceof Pawn) {
+                        FEN.append(piece.isWhite() ? "P" : "p");
+                    } else if (piece instanceof Knight) {
+                        FEN.append(piece.isWhite() ? "N" : "n");
+                    } else if (piece instanceof Bishop) {
+                        FEN.append(piece.isWhite() ? "B" : "b");
+                    } else if (piece instanceof Rook) {
+                        FEN.append(piece.isWhite() ? "R" : "r");
+                    } else if (piece instanceof Queen) {
+                        FEN.append(piece.isWhite() ? "Q" : "q");
+                    } else if (piece instanceof King) {
+                        FEN.append(piece.isWhite() ? "K" : "k");
+                    }
+                }
+            }
+            if (y != 7) FEN.append("/");
+        }
+
+        FEN.append(player.getIsWhite() ? " w" : " b")
+                .append(" -")
+                .append(" -")
+                .append(" 0")
+                .append(" 0");
+
+        return FEN.toString();
+    }
+}
