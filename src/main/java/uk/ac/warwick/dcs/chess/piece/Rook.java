893116fe7e02f3bf785553a642b4248d7185ecbb
ionorins <ion.orins@modex.tech>
2020-10-09 12:51:15 +0100

    rook

--- src/main/java/uk/ac/warwick/dcs/chess/piece/Rook.java
+++ src/main/java/uk/ac/warwick/dcs/chess/piece/Rook.java
@@ -9,6 +9,62 @@ public class Rook extends ChessPiece {
     @Override
     public Move[] getAvailableMoves() {
         availableMoves.clear();
+    
+        // down
+        int v = this.vertical;
+        int h = this.horizontal;
+
+        while (++v < 8) {
+            if (board.pieceAtLocation(v, h) == null)
+                availableMoves.add(new Move(this, v, h, false));
+            else {
+                if (board.pieceAtLocation(v, h).isWhite() != this.isWhite())
+                    availableMoves.add(new Move(this, v, h, true));
+                break;
+            }
+        }
+
+        // up
+        v = this.vertical;
+        h = this.horizontal;
+
+        while (--v >= 0) {
+            if (board.pieceAtLocation(v, h) == null)
+                availableMoves.add(new Move(this, v, h, false));
+            else {
+                if (board.pieceAtLocation(v, h).isWhite() != this.isWhite())
+                    availableMoves.add(new Move(this, v, h, true));
+                break;
+            }
+        }
+
+        // right
+        v = this.vertical;
+        h = this.horizontal;
+
+        while (++h < 8) {
+            if (board.pieceAtLocation(v, h) == null)
+                availableMoves.add(new Move(this, v, h, false));
+            else {
+                if (board.pieceAtLocation(v, h).isWhite() != this.isWhite())
+                    availableMoves.add(new Move(this, v, h, true));
+                break;
+            }
+        }
+
+        // left
+        v = this.vertical;
+        h = this.horizontal;
+
+        while (--h >= 0) {
+            if (board.pieceAtLocation(v, h) == null)
+                availableMoves.add(new Move(this, v, h, false));
+            else {
+                if (board.pieceAtLocation(v, h).isWhite() != this.isWhite())
+                    availableMoves.add(new Move(this, v, h, true));
+                break;
+            }
+        }
 
         return availableMoves.movesToArray();
     }
